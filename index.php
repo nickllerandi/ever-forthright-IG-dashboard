b56b463b722c49ca6b2d97c26b8a79f8e4ff47fa
asacarrington <asacarrington@gmail.com>
2015-12-19 23:03:44 +0000

    row fix

--- index.php
+++ index.php
@@ -7,29 +7,28 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>N. Llerandi</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,700" type="text/css">  
-    <link rel="stylesheet" href="css/style.css" type="text/css">  
-    <link rel="stylesheet" href="css/slideshow.css" type="text/css">  
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,700" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="css/slideshow.css" type="text/css">
   </head>
-    
+
   <body>
       <div class="container-fluid">
-          <ul class="nav">
-              <li><a href="#">In the Studio</a></li>
-              <li><a href="#">Music Academy</a></li>
-              <li><a href="#">Booze</a></li>
-          </ul>
+          <div class="row">
+            <div class="col-md-4"><a href="#">In the Studio</a></div>
+            <div class="col-md-4"><a href="#">Music Academy</a><</div>
+            <div class="col-md-4"><a href="#">Booze</a></div>
+          </div>
           <div class="row">
               <div class="col-md-12">
                   <img src="../img/lostbanner1700.jpg">
               </div>
           </div>
       </div>
-          
+
       <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
       <!-- Include all compiled plugins (below), or include individual files as needed -->
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
   </body>
 </html>
-
