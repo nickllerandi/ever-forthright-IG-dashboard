ccebda0131180f10a9f233fc10c16558636c0a6e
Nick <nicholasllerandi@gmail.com>
2015-12-19 17:58:22 -0500

    bootstrap pissin me off

--- index.php
+++ index.php
@@ -13,18 +13,12 @@
   </head>
     
   <body>
-      <div class="container">
-          <div class="row">
-              <div class="col-md-12">
-                  <ul class="nav nav-pills">
-                      <li class="inTheStudio"><a href="#">In the Studio</a></li>
-                      <li class="musicAcademy"><a href="#">Music Academy</a></li>
-                      <li class="booze"><a href="#">Booze</a></li>
-                  </ul>
-              </div>
-          </div>
-      </div>
       <div class="container-fluid">
+          <ul class="nav">
+              <li><a href="#">In the Studio</a></li>
+              <li><a href="#">Music Academy</a></li>
+              <li><a href="#">Booze</a></li>
+          </ul>
           <div class="row">
               <div class="col-md-12">
                   <img src="../img/lostbanner1700.jpg">
