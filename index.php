e2f211bb98a2775ae77a339d288fdd227b9dde65
Nick <nicholasllerandi@gmail.com>
2015-12-22 07:00:32 -0500

    added instagram feed. need to add 2 more using alternate hashashtags

--- index.php
+++ index.php
@@ -10,7 +10,12 @@
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,700" type="text/css">
     <link rel="stylesheet" href="css/style.css" type="text/css">
     <link rel="stylesheet" href="css/slideshow.css" type="text/css">
+      
+    <!-- instagram start -->  
+      
     <script type="text/javascript" src="instafeed.js/instafeed.min.js"></script>
+      
+      <!-- instagram stop -->
       
   </head>
 
@@ -31,21 +36,32 @@
           </div>         
       </div>
       
-      <div class="row">
-          <div class="col-md-12">
-              <div id="instafeed"></div>
-          </div>
+      <!-- instagram start -->
+      
+              <div class="container">
+      
+     <div class="row">
+         <div class="col-md-4">  
+         <div id="instafeed"></div>
+         </div>
+     </div>    
       </div>
-        
-      <script type="text/javascript">
+         
+       <script type="text/javascript">
     var feed = new Instafeed({
-        get: 'tagged',
-        tagName: 'awesome',
+        get: 'user',
+        userId: 231269882,
+                resolution: 'standard_resolution',
+        accessToken: '231269882.b5fe880.615795fc785a426f982d015761971415',
         clientId: 'b5fe88095947410699a2a913ee3dc8f9',
-        template: '<a href="{{link}}"><img src="{{image}}" /></a>'
+        filter: function(image) {
+            return image.tags.indexOf('efinthestudio') >= 0;
+        }
     });
     feed.run();
       </script>
+      
+      <!-- instagram stop -->
 
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
