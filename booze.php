39536cca75d22cc5c8e5dbac3a66739d8d1c343d
Nick <nicholasllerandi@gmail.com>
2015-12-29 10:44:57 -0500

    adding instafeed

---
+++ booze.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>N. Llerandi</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,700" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="css/slideshow.css" type="text/css">
+    <script type="text/javascript" src="instafeed.js/instafeed.min.js"></script>
+      
+  </head>
+    
+  <body>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-4"><a href="inthestudio.php">In the Studio</a></div>
+            <div class="col-md-4"><a href="musicacademy.php">Music Academy</a></div>
+            <div class="col-md-4"><a href="booze.php">Booze</a></div>
+          </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-4"></div>
+                <div class="col-md-4">  
+                    <div id="boozeTag"></div>
+                </div>
+            </div>    
+        </div>
+            
+            <script type="text/javascript">
+                var boozeTag = new Instafeed({
+                target: 'boozeTag',
+                get: 'user',
+                userId: 231269882,
+                resolution: 'standard_resolution',
+                accessToken: '231269882.b5fe880.615795fc785a426f982d015761971415',
+                filter: function(image) {
+                    return image.tags.indexOf('efbooze') >= 0;
+                }
+            });
+                boozeTag.run();
+            </script>
+            
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+ 
+  </body>
+</html>
